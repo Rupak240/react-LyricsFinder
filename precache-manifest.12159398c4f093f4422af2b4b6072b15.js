7857045a1578286caa4a7a8708136eb04e4417a3
RupaK <rupakdey1998@gmail.com>
2020-09-03 17:02:04 +0530

    Updates

---
+++ precache-manifest.12159398c4f093f4422af2b4b6072b15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1a09abcc71e2e13b801c48f859a545",
+    "url": "/react-LyricsFinder/index.html"
+  },
+  {
+    "revision": "feb0aba989df2ab91cb1",
+    "url": "/react-LyricsFinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2150f98a109a760af79a",
+    "url": "/react-LyricsFinder/static/js/2.d6ff4356.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/react-LyricsFinder/static/js/2.d6ff4356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb0aba989df2ab91cb1",
+    "url": "/react-LyricsFinder/static/js/main.1d708829.chunk.js"
+  },
+  {
+    "revision": "65561dccb0c6a8b8c789",
+    "url": "/react-LyricsFinder/static/js/runtime-main.4fb68cc9.js"
+  }
+]);
